d5a301908d71800d233c800d21abb5aa821c74ef
JRyanHenson <hensojr@gmail.com>
2017-07-31 21:12:22 -0600

    Added week 1 and started week 2 prodev HW.

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE-edge" />
+		<meta name=="viewport" content="width=device-width, initial-scale=1" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<title>Ryan Henson's Professional Development</title>
+	</head>
+	<body>
+		<!-- begin grid layout -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2 col-md-offset-10">
+					<h2>7/21/17</h2>
+				</div>
+				<div class="col-md-6">
+					<h2>Quantitative Accomplishments</h2>
+					<ul>
+						<li>As a project manager, oversaw the completion of multiple information security related contracts with an average annual revenue of 1.5 million dollars</li>
+						<li>Managed a team six information system security engineers in completion over 100 information security technical and programmatic assessments in accordance with the NIST RMF procedures</li>
+						<li>Oversaw the full accreditation of seven U.S. Marine Corps tactical information systems in accordance with the DoD RMF procedures</li>
+						<li>Over 10 years of combined military and civilian technical experience related to information assurance and computer network defense, computer network exploitation, and vulnerability assessments and vulnerability mitigation strategies implementation for tactical information systems</li>
+					</ul>
+				</div><!-- /.col-md-6-->
+				<div class="col-md-6">
+					<h2>Qualitative Accomplishments</h2>
+					<ul>
+						<li>Developed scripted tools and web based applications to automate the information security assessment and accreditation process greatly reducing the number of man-hours required to complete tasks</li>
+						<li>Provided subject matter expertise to system developers in the implementation of security engineering principals into the development process</li>
+						<li>Using various security tools and technical acumen, provided in-depth analysis of systems security posture culminating in risk determination and security control recommendations</li>
+						<li>Reviewed directives impacting customer’s systems and provided required documentation and/or action to remain compliant with applicable information system security policy</li>
+						<li>Created security checklists and test procedures to support specific evaluation events of customer systems</li>
+						<li>Provided input relating to information security issues, concerns, and recommendations throughout multiple phases of the system lifecycle including planning, pre-deployment, and post-deployment</li>
+					</ul>
+				</div><!-- /.col-md-6-->
+			</div><!-- /.row-->
+				<div class="row">
+					<div class="col-md-6">
+						<h2>Golden Circle</h2>
+						<p>What: Enhance my current career by integrating web development</p>
+						<p>How: Attend 10-week coding boot camp and do self-study </p>
+						<p>Why: To become a become a well-rounded technologist</p>
+					</div><!-- /.col-md-6-->
+					<div class="col-md-6">
+						<h2>Elevator Pitch</h2>
+						<p>Hi, my name is Slim Shad… I mean Ryan Henson. I’m an information security professional and developer. I’m currently working to develop web based tools to automate the NIST Risk Management Framework (RMF) assessment process.</p> 
+					</div><!-- /.col-md-6-->
+				</div><!-- /.row-->
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Career Buddy</h2>
+					<p>My career buddy is Tim Williams</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2 col-md-offset-10">
+					<h2>7/28/17</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h2><strong>Ryan Henson</strong></h2>
+					<p>4228 Courtney Ave NE	</p>
+					<p>Albuquerque, NM 87108</p>
+					<p>(505) 796-2961</p>
+				</div>
+				<div class="col-md-2 col-md-offset-6">
+					<br><br><br><br><br><br>
+					<a href="mailto:hensojr@gmail.com?Subject=I%Liked%Your%Resume" target="_top">hensojr@gmail.com</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h2><strong>Qualification Summary</strong></h2>
+					<ul>
+						<li>Dedicated professional with strong management, communication, and team building skills</li>
+						<li>Passionate and seasoned information security engineer</li>
+						<li>Experienced system and network administrator</li>
+						<li>Web application developer in PHP, HTML5, CSS, Java Script, and MySQL</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+				<h2><strong>Technical Skills</strong></h2>
+				<ul>
+					<li>Servers and OSs: Red Hat Server, Kali Linux, Ubuntu Linux, CISCO IOS, VMWare ESXi, Microsoft Windows Server</li>
+					<li>Tools: Nessus Security Center, Kali Security Suite, NMAP, Wireshark, Snort, Security Compliance Checker (SCC)</li>
+					<li>Databases and Webservers: MySQL, Apache</li>
+					<li>Languages: Bash, Batch, PowerShell, Python, C++, PHP, HTML5, CSS, git, and Java Script</li>
+					<li>Frameworks: Risk Management Framework (RMF)</li>
+				</ul>
+				</div>
+			</div>
+	</body>
+</html>
